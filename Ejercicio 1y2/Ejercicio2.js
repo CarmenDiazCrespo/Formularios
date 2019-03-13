03a9c3d9faf621be8e44d6d3853134dfa27ddda6
CarmenDiazCrespo <32709202+CarmenDiazCrespo@users.noreply.github.com>
2019-03-13 17:11:48 +0100

    Delete Ejercicio2.js

--- Ejercicio 1y2/Ejercicio2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function pasar(){
-    document.getElementById("submenu").style.display = "block";
-}
-function nopasar(){
-    document.getElementById("submenu").style.display = "none";
-}
